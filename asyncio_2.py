704977016078ac62ba713ee35be6a1566f041ac9
Wulong Cha <ziyean@outlook.com>
2020-08-12 13:55:12 +0800

    add more asyncio demos

--- asyncio_2.py
+++ asyncio_2.py
@@ -1,5 +1,5 @@
 import asyncio
-import random
+import random, time
 
 # ANSI colors
 c = (
@@ -14,17 +14,24 @@ async def makerandom(idx: int, threshold: int = 6) -> int:
     i = random.randint(0, 10)
     while i <= threshold:
         print(c[idx + 1] + f"makerandom({idx}) == {i} too low; retrying.")
+        # 小于一个threshold的话，跳出去执行其他的协程
         await asyncio.sleep(idx + 1)
         i = random.randint(0, 10)
     print(c[idx + 1] + f"---> Finished: makerandom({idx}) == {i}" + c[0])
     return i
 
 async def main():
-    res = await asyncio.gather(*(makerandom(i, 10 - i - 1) for i in range(3)))
+    # asyncio.gather(*aws, loop=None, ..)
+    # run awaitable objects in the aws sequence concurrently
+    # return: aggregate list of returned values
+    res = await asyncio.gather(*(makerandom(i, 10 - i - 1) for i in range(3))) 
     return res
 
 if __name__ == "__main__":
     random.seed(444)
+    # run: execute the coroutine and return the result
     r1, r2, r3 = asyncio.run(main())
     print()
     print(f"r1: {r1}, r2: {r2}, r3: {r3}")
+
+
